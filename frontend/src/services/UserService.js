b6241cb350d3677f3229f1035243e293c789dd23
Felipe Santiago <devfelipesantiago@gmail.com>
2025-03-02 20:39:29 -0300

    Feat: Service api connection

---
+++ frontend/src/services/UserService.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+
+const userService = {
+  list: async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_SERVER_URL}/users`
+      );
+      return response.data;
+    } catch (error) {
+      throw (
+        error.response?.data || {
+          message: 'An error occurred while fetching users',
+        }
+      );
+    }
+  },
+
+  get: async (id) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_SERVER_URL}/user/${id}`
+      );
+      return response.data;
+    } catch (error) {
+      throw (
+        error.response?.data || {
+          message: 'An error occurred while fetching user',
+        }
+      );
+    }
+  },
+
+  create: async (data) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/user/register`,
+        data
+      );
+      return response.data;
+    } catch (error) {
+      throw (
+        error.response?.data || {
+          message: 'An error occurred while creating user',
+        }
+      );
+    }
+  },
+
+  delete: async (id) => {
+    try {
+      await axios.delete(
+        `${process.env.REACT_APP_SERVER_URL}/user/delete/${id}`
+      );
+    } catch (error) {
+      throw (
+        error.response?.data || {
+          message: 'An error occurred while deleting user',
+        }
+      );
+    }
+  },
+  update: async (id, data) => {
+    try {
+      const response = await axios.put(
+        `${process.env.REACT_APP_SERVER_URL}/user/update/${id}`,
+        data
+      );
+      return response.data;
+    } catch (error) {
+      throw (
+        error.response?.data || {
+          message: 'An error occurred while updating user',
+        }
+      );
+    }
+  },
+};
+
+export default userService;
